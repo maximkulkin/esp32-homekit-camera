85152f23627eb96908a2d42a64b07c8ea3ed4d42
Maxim Kulkin <maxim.kulkin@gmail.com>
2018-12-20 16:23:46 +0400

    Implement camera service with snapshot support (no streaming yet)

--- main/app_main.c
+++ main/app_main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <esp_wifi.h>
 #include <esp_event_loop.h>
 #include <esp_log.h>
@@ -13,8 +14,16 @@
 
 #include <camera.h>
 
+#define STREAMING_STATUS_AVAILABLE 0
+#define STREAMING_STATUS_IN_USE 1
+#define STREAMING_STATUS_UNAVAILABLE 2
+
+
+#define TAG "esp32_camera"
+
+
 #define CAMERA_PIXEL_FORMAT CAMERA_PF_GRAYSCALE
-#define CAMERA_FRAME_SIZE CAMERA_FS_SVGA
+#define CAMERA_FRAME_SIZE CAMERA_FS_VGA
 static camera_pixelformat_t s_pixel_format;
 
 
@@ -69,6 +78,65 @@ void camera_led_set(bool on) {
     camera_led_on = on;
 }
 
+void camera_identify_task(void *_args) {
+    for (int i=0; i<3; i++) {
+        for (int j=0; j<2; j++) {
+            camera_led_set(true);
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+            camera_led_set(false);
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+        }
+
+        vTaskDelay(250 / portTICK_PERIOD_MS);
+    }
+
+    camera_led_set(true);
+
+    vTaskDelete(NULL);
+}
+
+void camera_identify(homekit_value_t _value) {
+    printf("Camera identify\n");
+    xTaskCreate(camera_identify_task, "Camera identify", 512, NULL, 2, NULL);
+}
+
+homekit_value_t camera_streaming_status_get() {
+    tlv_values_t *tlv = tlv_new();
+    tlv_add_integer_value(tlv, 1, STREAMING_STATUS_AVAILABLE);
+    return HOMEKIT_TLV(tlv);
+}
+
+homekit_value_t camera_setup_endpoints_get() {
+    return HOMEKIT_TLV(tlv_new());
+}
+
+void camera_setup_endpoints_set(homekit_value_t value) {
+    if (value.format != homekit_format_bool) {
+        printf("Invalid value format: %d\n", value.format);
+        return;
+    }
+
+    // TODO:
+}
+
+homekit_value_t camera_selected_rtp_configuration_get() {
+    return HOMEKIT_TLV(tlv_new());
+}
+
+void camera_selected_rtp_configuration_set(homekit_value_t value) {
+    if (value.format != homekit_format_bool) {
+        printf("Invalid value format: %d\n", value.format);
+        return;
+    }
+
+    // TODO:
+}
+
+tlv_values_t supported_video_config = {};
+tlv_values_t supported_audio_config = {};
+tlv_values_t supported_rtp_config = {};
+
+
 void camera_accessory_init() {
     ESP_ERROR_CHECK(gpio_install_isr_service(0));
     gpio_set_direction(camera_led_gpio, GPIO_MODE_OUTPUT);
@@ -98,73 +166,95 @@ void camera_accessory_init() {
     camera_model_t camera_model;
     esp_err_t err = camera_probe(&camera_config, &camera_model);
     if (err != ESP_OK) {
-        ESP_LOGE("esp32_camera", "Camera probe failed with error 0x%x", err);
+        ESP_LOGE(TAG, "Camera probe failed with error 0x%x", err);
         return;
     }
 
     if (camera_model == CAMERA_OV7725) {
         s_pixel_format = CAMERA_PIXEL_FORMAT;
         camera_config.frame_size = CAMERA_FRAME_SIZE;
-        ESP_LOGI("esp32_camera", "Detected OV7725 camera, using %s bitmap format",
-                CAMERA_PIXEL_FORMAT == CAMERA_PF_GRAYSCALE ?
-                        "grayscale" : "RGB565");
+        ESP_LOGI(TAG, "Detected OV7725 camera, using %s bitmap format",
+                 CAMERA_PIXEL_FORMAT == CAMERA_PF_GRAYSCALE ?  "grayscale" : "RGB565");
     } else if (camera_model == CAMERA_OV2640) {
-        ESP_LOGI("esp32_camera", "Detected OV2640 camera, using JPEG format");
+        ESP_LOGI(TAG, "Detected OV2640 camera, using JPEG format");
         s_pixel_format = CAMERA_PF_JPEG;
         camera_config.frame_size = CAMERA_FRAME_SIZE;
         camera_config.jpeg_quality = 15;
     } else {
-        ESP_LOGE("esp32_camera", "Camera not supported");
+        ESP_LOGE(TAG, "Camera not supported");
         return;
     }
 
     camera_config.pixel_format = s_pixel_format;
     err = camera_init(&camera_config);
     if (err != ESP_OK) {
-        ESP_LOGE("esp32_camera", "Camera init failed with error 0x%x", err);
-        return;
-    }
-}
-
-void camera_identify_task(void *_args) {
-    for (int i=0; i<3; i++) {
-        for (int j=0; j<2; j++) {
-            camera_led_set(true);
-            vTaskDelay(100 / portTICK_PERIOD_MS);
-            camera_led_set(false);
-            vTaskDelay(100 / portTICK_PERIOD_MS);
-        }
-
-        vTaskDelay(250 / portTICK_PERIOD_MS);
-    }
-
-    camera_led_set(true);
-
-    vTaskDelete(NULL);
-}
-
-void camera_identify(homekit_value_t _value) {
-    printf("Camera identify\n");
-    xTaskCreate(camera_identify_task, "Camera identify", 512, NULL, 2, NULL);
-}
-
-homekit_value_t camera_led_on_get() {
-    return HOMEKIT_BOOL(camera_led_on);
-}
-
-void camera_led_on_set(homekit_value_t value) {
-    if (value.format != homekit_format_bool) {
-        printf("Invalid value format: %d\n", value.format);
+        ESP_LOGE(TAG, "Camera init failed with error 0x%x", err);
         return;
     }
 
-    camera_led_on = value.bool_value;
-    camera_led_set(camera_led_on);
+    tlv_values_t *video_codec_params = tlv_new();
+    tlv_add_integer_value(video_codec_params, 1, 1);  // Profile ID
+    tlv_add_integer_value(video_codec_params, 2, 0);  // Level
+    tlv_add_integer_value(video_codec_params, 3, 0);  // Packetization mode
+    tlv_add_integer_value(video_codec_params, 4, 0);  // CVO Enabled
+    tlv_add_integer_value(video_codec_params, 5, 1);  // CVO ID
+
+    tlv_values_t *video_attributes = tlv_new();
+    tlv_add_integer_value(video_attributes, 1, 640);  // Image width
+    tlv_add_integer_value(video_attributes, 2, 480);  // Image height
+    tlv_add_integer_value(video_attributes, 3, 30);  // Frame rate
+
+    tlv_values_t *video_codec_config = tlv_new();
+    tlv_add_integer_value(video_codec_config, 1, 0);  // Video codec type
+    tlv_add_tlv_value(video_codec_config, 2, video_codec_params);  // Video codec params
+    tlv_add_tlv_value(video_codec_config, 3, video_attributes);  // Video attributes
+
+    tlv_add_tlv_value(&supported_video_config, 1, video_codec_config);
+
+
+    tlv_values_t *audio_codec_params = tlv_new();
+    tlv_add_integer_value(audio_codec_params, 1, 1);
+    tlv_add_integer_value(audio_codec_params, 2, 0);
+    tlv_add_integer_value(audio_codec_params, 3, 0);
+
+    tlv_values_t *audio_codec = tlv_new();
+    tlv_add_integer_value(audio_codec, 1, 3);
+    tlv_add_tlv_value(audio_codec, 2, audio_codec_params);
+
+    tlv_add_integer_value(&supported_rtp_config, 2, 2);  // SRTP crypto suite
+}
+
+
+void camera_on_resource(client_context_t *context) {
+    char *body = (char*) homekit_client_get_request_body(context);
+    body[homekit_client_get_request_body_size(context)] = 0;
+    ESP_LOGI(TAG, "Resource payload: %s", body);
+
+    esp_err_t err = camera_run();
+    if (err != ESP_OK) {
+        ESP_LOGD(TAG, "Camera capture failed with error = %d", err);
+        static unsigned char error_payload[] = "HTTP/1.1 500 Camera capture error\r\n\r\n";
+        homekit_client_send(context, error_payload, sizeof(error_payload)-1);
+    }
+
+    static unsigned char success_payload[] =
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: image/jpeg\r\n"
+        "Content-Disposition: inline; filename=capture.jpg\r\n"
+        "Content-Length: ";
+
+    homekit_client_send(context, success_payload, sizeof(success_payload)-1);
+
+    char buffer[16];
+    snprintf(buffer, sizeof(buffer), "%d\r\n\r\n", camera_get_data_size());
+
+    homekit_client_send(context, (unsigned char*) buffer, strlen(buffer));
+    homekit_client_send(context, camera_get_fb(), camera_get_data_size());
 }
 
 
 homekit_accessory_t *accessories[] = {
-    HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_lightbulb, .services=(homekit_service_t*[]){
+    HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_ip_camera, .services=(homekit_service_t*[]){
         HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]){
             HOMEKIT_CHARACTERISTIC(NAME, "M5Stack Camera"),
             HOMEKIT_CHARACTERISTIC(MANUFACTURER, "HaPK"),
@@ -174,12 +264,30 @@ homekit_accessory_t *accessories[] = {
             HOMEKIT_CHARACTERISTIC(IDENTIFY, camera_identify),
             NULL
         }),
-        HOMEKIT_SERVICE(LIGHTBULB, .primary=true, .characteristics=(homekit_characteristic_t*[]){
-            HOMEKIT_CHARACTERISTIC(NAME, "Camera LED"),
+        HOMEKIT_SERVICE(CAMERA_RTP_STREAM_MANAGEMENT, .primary=true, .characteristics=(homekit_characteristic_t*[]){
+            HOMEKIT_CHARACTERISTIC(NAME, "Camera"),
+            HOMEKIT_CHARACTERISTIC(STREAMING_STATUS, .getter=camera_streaming_status_get),
             HOMEKIT_CHARACTERISTIC(
-                ON, false,
-                .getter=camera_led_on_get,
-                .setter=camera_led_on_set
+                SUPPORTED_VIDEO_STREAM_CONFIGURATION,
+                .value=HOMEKIT_TLV_(&supported_video_config)
+            ),
+            HOMEKIT_CHARACTERISTIC(
+                SUPPORTED_AUDIO_STREAM_CONFIGURATION,
+                .value=HOMEKIT_TLV_(&supported_audio_config)
+            ),
+            HOMEKIT_CHARACTERISTIC(
+                SUPPORTED_RTP_CONFIGURATION,
+                .value=HOMEKIT_TLV_(&supported_rtp_config)
+            ),
+            HOMEKIT_CHARACTERISTIC(
+                SETUP_ENDPOINTS,
+                .getter=camera_setup_endpoints_get,
+                .setter=camera_setup_endpoints_set
+            ),
+            HOMEKIT_CHARACTERISTIC(
+                SELECTED_RTP_STREAM_CONFIGURATION,
+                .getter=camera_selected_rtp_configuration_get,
+                .setter=camera_selected_rtp_configuration_set
             ),
             NULL
         }),
@@ -188,14 +296,18 @@ homekit_accessory_t *accessories[] = {
     NULL
 };
 
+
 homekit_server_config_t config = {
     .accessories = accessories,
-    .password = "111-11-111"
+    .password = "111-11-111",
+    .on_resource = camera_on_resource
 };
+
 
 void on_wifi_ready() {
     homekit_server_init(&config);
 }
+
 
 void app_main(void) {
     // Initialize NVS
