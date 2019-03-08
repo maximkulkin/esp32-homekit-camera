d5ab0c8a226c0dd43ab4f2bad458c5448f6c5882
Maxim Kulkin <maxim.kulkin@gmail.com>
2019-03-08 03:49:00 -0800

    Patch x264 for more compact memory usage

--- components/x264/x264-snapshot-20181221-2245-stable/encoder/encoder.c
+++ components/x264/x264-snapshot-20181221-2245-stable/encoder/encoder.c
@@ -1615,7 +1615,7 @@ x264_t *x264_encoder_open( x264_param_t *param )
     }
 
     h->out.i_nal = 0;
-    h->out.i_bitstream = X264_MAX( 1000000, h->param.i_width * h->param.i_height * 4
+    h->out.i_bitstream = X264_MAX( 100000, h->param.i_width * h->param.i_height * 4
         * ( h->param.rc.i_rc_method == X264_RC_ABR ? pow( 0.95, h->param.rc.i_qp_min )
           : pow( 0.95, h->param.rc.i_qp_constant ) * X264_MAX( 1, h->param.rc.f_ip_factor )));
 
