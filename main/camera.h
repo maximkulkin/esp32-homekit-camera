fe5e02564361736c43033407a9ec7aabc46d6e33
Maxim Kulkin <maxim.kulkin@gmail.com>
2019-09-16 15:07:02 -0700

    Add DIYMore camera module pin configuration

--- main/camera.h
+++ main/camera.h
@@ -114,6 +114,25 @@
 #define CAMERA_PIN_HREF     25
 #define CAMERA_PIN_PCLK     19
 
+#elif CONFIG_CAMERA_MODEL_DIYMORE
+#define CAMERA_PIN_PWDN     32
+#define CAMERA_PIN_RESET     2
+#define CAMERA_PIN_SIOD     26
+#define CAMERA_PIN_SIOC     27
+#define CAMERA_PIN_XCLK      0
+
+#define CAMERA_PIN_D0        5
+#define CAMERA_PIN_D1       18
+#define CAMERA_PIN_D2       19
+#define CAMERA_PIN_D3       21
+#define CAMERA_PIN_D4       36
+#define CAMERA_PIN_D5       39
+#define CAMERA_PIN_D6       34
+#define CAMERA_PIN_D7       35
+#define CAMERA_PIN_VSYNC    25
+#define CAMERA_PIN_HREF     23
+#define CAMERA_PIN_PCLK     22
+
 
 #elif CONFIG_CAMERA_MODEL_CUSTOM
 #define CAMERA_PIN_PWDN    CONFIG_CAMERA_PIN_PWDN
