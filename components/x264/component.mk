2162d2bf9b8e0bdc9550b8951e6a94c9d85977d3
Maxim Kulkin <maxim.kulkin@gmail.com>
2019-10-14 02:14:16 -0700

    Update x264 component to enable CFLAGS only for new gcc

--- components/x264/component.mk
+++ components/x264/component.mk
@@ -4,7 +4,9 @@ x264_THIRDPARTY_ROOT = x264-snapshot-20181221-2245-stable
 COMPONENT_SRCDIRS = $(x264_THIRDPARTY_ROOT)/common $(x264_THIRDPARTY_ROOT)/encoder
 COMPONENT_ADD_INCLUDEDIRS = . $(x264_THIRDPARTY_ROOT)
 
+ifneq ($(shell $(CC) -v 2>&1 | grep 'gcc version [^0-7]'),)
 CFLAGS += -Wno-error=format-truncation=
+endif
 
 $(eval $(call compile_exclude, $(addprefix $(x264_THIRDPARTY_ROOT)/, \
     common/opencl.o \
